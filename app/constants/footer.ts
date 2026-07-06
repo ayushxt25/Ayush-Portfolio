9c5e4e9975172d518641c71da10dfe1f75ed3054
Ayush <ayushgiri2125@gmail.com>
2026-07-06 10:34:52 +0530

    Revert "Fix public asset paths for deployment"
    This reverts commit 6858528d85c62a6e2628eeaa750fd3d97efdafea.

--- app/constants/footer.ts
+++ app/constants/footer.ts
@@ -4,31 +4,31 @@ export const FOOTER_LINKS: FooterLink[] = [
   {
     name: 'LinkedIn',
     hoverText: 'Connect with me',
-    icon: '/icons/linkedin.svg',
+    icon: 'icons/linkedin.svg',
     url: 'https://www.linkedin.com/in/ayush-giri-04544a348/',
   },
   {
     name: 'GitHub',
     hoverText: 'Open Sourcing',
-    icon: '/icons/github.svg',
+    icon: 'icons/github.svg',
     url: 'https://github.com/ayushxt25',
   },
   {
     name: 'Apple Music',
     hoverText: 'Curated playlists',
-    icon: '/icons/apple-music.svg',
+    icon: 'icons/apple-music.svg',
     url: 'https://music.apple.com/in/library/playlist/p.pmrAermhNxMO5xR',
   },
   {
     name: 'Instagram',
     hoverText: '@ayushhxttt',
-    icon: '/icons/instagram.svg',
+    icon: 'icons/instagram.svg',
     url: 'https://www.instagram.com/ayushhxttt/?hl=en',
   },
   {
     name: 'Resume',
     hoverText: 'Download',
-    icon: '/icons/file.svg',
-    url: '/Ayush_SE_Current.pdf',
+    icon: 'icons/file.svg',
+    url: './Ayush_SE_Current.pdf',
   }
 ];
